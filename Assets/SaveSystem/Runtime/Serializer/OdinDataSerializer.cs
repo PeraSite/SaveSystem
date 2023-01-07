41d8e3aa11326e558e0238e7be5cea28b26ebbdb
PeraSite <pretocki3@gmail.com>
2023-01-07 20:55:59 +0900

    feat: OdinDataSerializer에 DataFormat 설정 기능 추가

--- Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
+++ Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
@@ -1,20 +1,23 @@
 ﻿#if ODIN_INSPECTOR
 using System.Text;
 using Sirenix.Serialization;
+using UnityEngine;
 using Zenject;
 
 namespace SaveSystem.Runtime {
 	public class OdinDataSerializer : ScriptableObjectInstaller<OdinDataSerializer>, IDataSerializer {
+		[SerializeField] private DataFormat _dataFormat;
+
 		public string Serialize<T>(T value) {
-			return Encoding.UTF8.GetString(SerializationUtility.SerializeValue(value, DataFormat.JSON));
+			return Encoding.UTF8.GetString(SerializationUtility.SerializeValue(value, _dataFormat));
 		}
 
 		public T Deserialize<T>(string text) {
-			return SerializationUtility.DeserializeValue<T>(Encoding.UTF8.GetBytes(text), DataFormat.JSON);
+			return SerializationUtility.DeserializeValue<T>(Encoding.UTF8.GetBytes(text), _dataFormat);
 		}
 
 		public object Deserialize(string text) {
-			return SerializationUtility.DeserializeValueWeak(Encoding.UTF8.GetBytes(text), DataFormat.JSON);
+			return SerializationUtility.DeserializeValueWeak(Encoding.UTF8.GetBytes(text), _dataFormat);
 		}
 
 		public override void InstallBindings() {
