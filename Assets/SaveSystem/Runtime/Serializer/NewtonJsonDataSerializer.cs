aa72496a9bbfa4f6bb7b3aa950732ced7d3094c5
PeraSite <pretocki3@gmail.com>
2023-01-07 19:54:39 +0900

    refactor: Newtonsoft JSON Data Serializer가 primitive 값도 저장할 수 있게 Wrapper 기능 추가

--- Assets/SaveSystem/Runtime/Serializer/NewtonJsonDataSerializer.cs
+++ Assets/SaveSystem/Runtime/Serializer/NewtonJsonDataSerializer.cs
@@ -3,16 +3,21 @@ using Zenject;
 
 namespace SaveSystem.Runtime {
 	public class NewtonJsonDataSerializer : ScriptableObjectInstaller<NewtonJsonDataSerializer>, IDataSerializer {
+		private struct Wrapper<T> {
+			public T Value;
+		}
+
 		public string Serialize<T>(T value) {
-			return JsonConvert.SerializeObject(value);
+			var wrapped = new Wrapper<T> {Value = value};
+			return JsonConvert.SerializeObject(wrapped);
 		}
 
 		public T Deserialize<T>(string text) {
-			return JsonConvert.DeserializeObject<T>(text);
+			return JsonConvert.DeserializeObject<Wrapper<T>>(text).Value;
 		}
 
 		public object Deserialize(string text) {
-			return JsonConvert.DeserializeObject(text);
+			return JsonConvert.DeserializeObject<Wrapper<object>>(text).Value;
 		}
 
 		public override void InstallBindings() {
