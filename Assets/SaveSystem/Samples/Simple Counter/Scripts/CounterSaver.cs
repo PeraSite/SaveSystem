26a267a41fb9177cf1ed3097746a76b45bec5680
PeraSite <pretocki3@gmail.com>
2023-01-07 15:45:03 +0900

    feat: 데모에 새로운 기능 적용

---
+++ Assets/SaveSystem/Samples/Simple Counter/Scripts/CounterSaver.cs
@@ -0,0 +1,26 @@
+﻿using SaveSystem.Runtime;
+using TMPro;
+using UnityEngine;
+
+namespace SaveSystem.Samples {
+	public class CounterSaver : Saver<int> {
+		[SerializeField] private TextMeshProUGUI _text;
+		[SerializeField] private int _value;
+
+		public void Increase() {
+			_value++;
+		}
+
+		public override void ResetData() {
+			_value = 0;
+		}
+
+		public override void ApplyData(int data) {
+			_value = data;
+		}
+
+		public override int SaveData() {
+			return _value;
+		}
+	}
+}
---
+++ Assets/SaveSystem/Samples/Simple Counter/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SaveSystem.Runtime;
+using UnityEngine;
+using Zenject;
+
+namespace SaveSystem.Samples {
+	public class Test : MonoBehaviour {
+		public SaveManager _manager;
+
+		private void Awake() {
+			Debug.Log("awake");
+		}
+
+		private void Start() {
+			Debug.Log("start");
+		}
+
+		[Inject]
+		public void Construct(SaveManager manager) {
+			_manager = manager;
+		}
+	}
+}
