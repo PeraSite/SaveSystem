b5aee3c42fe052880b03cae00bf74d7fb2b00bb8
PeraSite <pretocki3@gmail.com>
2023-01-07 17:53:42 +0900

    feat: UnityEngine.JsonUtility를 Newtonsoft.Json.JsonConvert로 변경

---
+++ Assets/SaveSystem/Runtime/Serializer/NewtonJsonDataSerializer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Zenject;
+
+namespace SaveSystem.Runtime {
+	public class NewtonJsonDataSerializer : ScriptableObjectInstaller<NewtonJsonDataSerializer>, IDataSerializer {
+		public string Serialize<T>(T value) {
+			return JsonConvert.SerializeObject(value);
+		}
+
+		public T Deserialize<T>(string text) {
+			return JsonConvert.DeserializeObject<T>(text);
+		}
+
+		public object Deserialize(string text) {
+			return JsonConvert.DeserializeObject(text);
+		}
+
+		public override void InstallBindings() {
+			Container.BindInterfacesAndSelfTo<NewtonJsonDataSerializer>().FromInstance(this).AsSingle();
+		}
+	}
+}
