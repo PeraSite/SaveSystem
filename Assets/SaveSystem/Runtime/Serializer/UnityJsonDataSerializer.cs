121ac72848e3a4362d1b3bcd44f9e98ec3dc1c1e
PeraSite <pretocki3@gmail.com>
2023-01-07 16:49:08 +0900

    feat: UnityEngine.JsonUtility를 사용하는 기본 DataSerializer 추가

---
+++ Assets/SaveSystem/Runtime/Serializer/UnityJsonDataSerializer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace SaveSystem.Runtime {
+	public class UnityJsonDataSerializer : ScriptableObjectInstaller<UnityJsonDataSerializer>, IDataSerializer {
+		public string Serialize<T>(T value) {
+			return JsonUtility.ToJson(value);
+		}
+
+		public T Deserialize<T>(string text) {
+			return JsonUtility.FromJson<T>(text);
+		}
+
+		public object Deserialize(string text) {
+			return JsonUtility.FromJson<object>(text);
+		}
+
+		public override void InstallBindings() {
+			Container.BindInterfacesAndSelfTo<UnityJsonDataSerializer>().FromInstance(this).AsSingle();
+		}
+	}
+}
