57439fcb5454f15e250a24313cc8f7cd89ef2cd0
PeraSite <pretocki3@gmail.com>
2023-01-08 01:48:38 +0900

    fix: text가 null일 때 대응 추가

--- Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
+++ Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
@@ -1,6 +1,7 @@
 ﻿#if ODIN_INSPECTOR
 using System.Text;
 using Sirenix.Serialization;
+using Sirenix.Utilities;
 using UnityEngine;
 using Zenject;
 
@@ -13,10 +14,12 @@ namespace SaveSystem.Runtime {
 		}
 
 		public T Deserialize<T>(string text) {
+			if (text.IsNullOrWhitespace()) return default;
 			return SerializationUtility.DeserializeValue<T>(Encoding.UTF8.GetBytes(text), _dataFormat);
 		}
 
 		public object Deserialize(string text) {
+			if (text.IsNullOrWhitespace()) return null;
 			return SerializationUtility.DeserializeValueWeak(Encoding.UTF8.GetBytes(text), _dataFormat);
 		}
 
