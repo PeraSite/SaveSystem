8b246dea18e90baef71e95f9f6f28ca744c53f6f
PeraSite <pretocki3@gmail.com>
2023-01-08 14:00:50 +0900

    refactor: ISaver의 Generics를 더 명확한 이름인 TValue로 변경

--- Assets/SaveSystem/Runtime/Saver/ISaver.cs
+++ Assets/SaveSystem/Runtime/Saver/ISaver.cs
@@ -11,19 +11,19 @@ namespace SaveSystem.Runtime {
 		public void ResetData();
 	}
 
-	public interface ISaver<T> : ISaver {
-		public void ApplyData(T data);
+	public interface ISaver<TValue> : ISaver {
+		public void ApplyData(TValue data);
 
-		public T SaveData();
+		public TValue SaveData();
 
 		void ISaver.ApplyDataWeak(object data) {
 			// 데이터 직렬화 과정에서 int를 long으로 변환하는 문제 대응
-			if (data is long && typeof(T) == typeof(int)) {
-				ApplyData((T) (object) Convert.ToInt32(data));
+			if (data is long && typeof(TValue) == typeof(int)) {
+				ApplyData((TValue) (object) Convert.ToInt32(data));
 				return;
 			}
 
-			ApplyData((T) data);
+			ApplyData((TValue) data);
 		}
 
 		object ISaver.SaveDataWeak() {
