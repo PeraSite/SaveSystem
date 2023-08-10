294cb327e64258991f5f8c4590c8bdf8dcee5e41
PeraSite <pretocki3@gmail.com>
2023-08-10 22:45:58 +0900

    feat: Inventory 데모 완료

--- Assets/SaveSystem/Samples~/Demo/Scripts/SaveSystemInstaller.cs
+++ Assets/SaveSystem/Samples~/Demo/Scripts/SaveSystemInstaller.cs
@@ -1,18 +1,22 @@
 ﻿using SaveSystem.Runtime;
+using SaveSystem.Samples.Serialization;
+using Sirenix.Serialization;
 using UnityEngine;
 using Zenject;
 
 namespace SaveSystem.Samples {
 	public class SaveSystemInstaller : MonoInstaller<SaveSystemInstaller> {
+		[SerializeField] private DataFormat _dataFormat;
 		[SerializeField] private CanvasGroup _fade;
 		[SerializeField] private float _animationTime;
 
 		public override void InstallBindings() {
 			// Base
-			Container.BindInterfacesTo<OdinDataSerializer>().AsSingle().NonLazy();
+			Container.BindInterfacesTo<OdinDataSerializer>().AsSingle().WithArguments(_dataFormat).NonLazy();
 			Container.BindInterfacesTo<PlayerPrefDataStorage>().AsSingle().NonLazy();
 			Container.BindInterfacesTo<FadeSceneTransition>().AsSingle().WithArguments(_fade, _animationTime).NonLazy();
 			Container.Bind<SceneTransitionManager>().AsSingle().NonLazy();
+			Container.BindInterfacesTo<AddressableReferenceResolver>().AsSingle().NonLazy();
 
 			// Scope
 			BindScope<GlobalScope>();
---
+++ Assets/SaveSystem/Samples~/Demo/Scripts/Serialization/AddressableReferenceResolver.cs
@@ -0,0 +1,46 @@
+﻿#if ODIN_INSPECTOR
+using System.Linq;
+using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace SaveSystem.Samples.Serialization {
+	public class AddressableReferenceResolver : IExternalStringReferenceResolver {
+		public IExternalStringReferenceResolver NextResolver { get; set; }
+
+		public bool TryResolveReference(string id, out object value) {
+			var locations = Addressables.LoadResourceLocationsAsync(id).WaitForCompletion();
+
+			if (locations.Count == 0) {
+				value = null;
+				return false;
+			}
+
+			var location = locations.First();
+			var loadAsset = Addressables.LoadAssetAsync<ScriptableObject>(location).WaitForCompletion();
+
+			value = loadAsset;
+			return true;
+		}
+
+		public bool CanReference(object value, out string id) {
+			if (value is ScriptableObject unityObject) {
+				id = unityObject.name;
+
+				var locations = Addressables.LoadResourceLocationsAsync(unityObject.name).WaitForCompletion();
+
+				// 만약 Addressable Asset을 찾았다면 true
+				if (locations.Count != 0) return true;
+
+				Debug.LogError($"Can't find reference to {unityObject.name} from Addressable!");
+				return false;
+
+			}
+
+			id = null;
+			return false;
+		}
+	}
+}
+
+  #endif
