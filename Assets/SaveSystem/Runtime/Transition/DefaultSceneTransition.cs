b792e53c1a1e843c2cd879293b690646958e8b03
PeraSite <pretocki3@gmail.com>
2023-08-10 20:46:17 +0900

    chore: 아무 효과 없는 DefaultSceneTransition 추가

---
+++ Assets/SaveSystem/Runtime/Transition/DefaultSceneTransition.cs
@@ -0,0 +1,13 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace SaveSystem.Runtime {
+	public class DefaultSceneTransition : ISceneTransition {
+		public UniTask StartTransition() {
+			return UniTask.CompletedTask;
+		}
+
+		public UniTask EndTransition() {
+			return UniTask.CompletedTask;
+		}
+	}
+}
