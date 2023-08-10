e4931f537efef6e798b9924da2e568412127170e
PeraSite <pretocki3@gmail.com>
2023-08-10 20:44:32 +0900

    refactor: Saver 관련 스크립트 폴더 정리

---
+++ Assets/SaveSystem/Samples~/Demo/Scripts/Saver/InventorySaver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SaveSystem.Runtime;
+
+namespace SaveSystem.Samples {
+	[Serializable]
+	public struct Inventory {
+		public List<Item> Items;
+	}
+
+	public class InventorySaver : Saver<Inventory, SlotScope> {
+		public Inventory Inventory;
+
+		public override void ApplyData(Inventory data) {
+			Inventory = data;
+		}
+
+		public override Inventory SaveData() {
+			return Inventory;
+		}
+	}
+}
