488d316f9d3658f699c2a360683a558050288cfa
PeraSite <pretocki3@gmail.com>
2023-08-10 22:45:29 +0900

    feat: OdinDataSerializer가 IExternalStringReferenceResolver를 Inject받아 사용하게 수정

--- Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
+++ Assets/SaveSystem/Runtime/Serializer/OdinDataSerializer.cs
@@ -6,21 +6,47 @@ using Zenject;
 
 namespace SaveSystem.Runtime {
 	public class OdinDataSerializer : IDataSerializer {
-		[InjectOptional]
-		private DataFormat _dataFormat = DataFormat.JSON;
+		private DataFormat _dataFormat;
+		private IExternalStringReferenceResolver _referenceResolver;
 
 		public string Serialize<T>(T value) {
-			return Encoding.UTF8.GetString(SerializationUtility.SerializeValue(value, _dataFormat));
+			var context = new SerializationContext {
+				StringReferenceResolver = _referenceResolver
+			};
+
+			var serializedBytes = SerializationUtility.SerializeValue(value, _dataFormat, context);
+			return Encoding.UTF8.GetString(serializedBytes);
 		}
 
 		public T Deserialize<T>(string text) {
 			if (text.IsNullOrWhitespace()) return default;
-			return SerializationUtility.DeserializeValue<T>(Encoding.UTF8.GetBytes(text), _dataFormat);
+
+			var context = new DeserializationContext {
+				StringReferenceResolver = _referenceResolver
+			};
+
+			var serializedBytes = Encoding.UTF8.GetBytes(text);
+			return SerializationUtility.DeserializeValue<T>(serializedBytes, _dataFormat, context);
 		}
 
 		public object Deserialize(string text) {
 			if (text.IsNullOrWhitespace()) return null;
-			return SerializationUtility.DeserializeValueWeak(Encoding.UTF8.GetBytes(text), _dataFormat);
+
+			var context = new DeserializationContext {
+				StringReferenceResolver = _referenceResolver
+			};
+
+			var serializedBytes = Encoding.UTF8.GetBytes(text);
+			return SerializationUtility.DeserializeValueWeak(serializedBytes, _dataFormat, context);
+		}
+
+		[Inject]
+		private void Construct(
+			IExternalStringReferenceResolver referenceResolver = null,
+			DataFormat dataFormat = DataFormat.JSON
+		) {
+			_referenceResolver = referenceResolver;
+			_dataFormat = dataFormat;
 		}
 	}
 }
